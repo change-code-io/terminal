c19fbd475bddee5a9cf0d11014c9d85ab8357261
Chris Georgen <c-georgen@users.noreply.github.com>
2024-02-02 21:33:16 -0600

    final cleanup for publication

--- docs/index.js
+++ docs/index.js
@@ -9,15 +9,15 @@ var App = {
     },
 
     ls: function() {
-        this.error('<ls> is not available. I guess you want to list available commands with <start>:', {keepWords: true});
+        this.error("<ls> is not available. I'm guessing you want to list available commands with <start>:", {keepWords: true});
          showStart(this);
     },
     contact: function() {
         this.echo("");
-        this.echo("If you want to get in touch, you can send us an email at <a href='mailto:weare@changecode.io'>weare@changecode.io</a> or book some time with us <a href= >here</a>", {keepWords: true})
+        this.echo("To get in touch, send us an email at <a href='mailto:weare@changecode.io'>weare@changecode.io</a> or book some time with us <a href=https://cal.com/team/change-code/introductory-call, target=_blank>here</a>.", {raw: true}, {keepWords: true})
     },
     founders: function() {
-        this.echo("Change Code was founded by Erin Murphy and Chris Georgen. Erin and Chris met while working at Topl, one of only a handful of blockchain protocols built specifically to power ReFi, social enterprise, and adoption in the Global South.\n\nChris has been active in web3 since 2012 and before starting Change Code, founded two startups in the space—first developing a mobile payments platform on Ethereum in 2015 and then founding Topl in 2017. Chris began programming while at Rice University as part of his research in computation physics, earning him 3 co-authored publications. \n\nBefore joining Topl, Erin worked in development finance for 10 years across 6 countries including Ukraine and India. For the UN Development Programme and others, Erin focused on micro-enterprise development, infrastructure financing, and women’s financial inclusion. In 2021, Erin graduated from the Wharton School and Lauder Institute with her MBA-MA.", {keepWords: true})
+        this.echo("Change Code was founded by Erin Murphy and Chris Georgen. Erin and Chris met while working at Topl, one of only a handful of blockchain protocols built specifically to power ReFi, social enterprise, and economic change in the Global South.\n\nChris has been active in web3 since 2012. Before starting Change Code, founded two startups in the space—first developing a mobile payments platform on Ethereum in 2015 and then founding Topl in 2017. Chris began programming while at Rice University as part of his research in computational physics, earning him 3 co-authored publications. \n\nBefore joining Topl, Erin worked in development finance for 10 years across 6 countries including Ukraine and India. For the UN Development Programme and others, Erin focused on micro-enterprise development, infrastructure financing, and women’s financial inclusion. In 2021, Erin graduated from the Wharton School and Lauder Institute with her MBA-MA.", {keepWords: true})
     },
     keywords: function() {
         this.echo("social impact, ReFi, blended finance, web3, social enterprise, sustainability \n\nMaybe this will help with SEO", {keepWords: true});
@@ -26,13 +26,13 @@ var App = {
         this.clear()
     },
     sudo: function() {
-        this.echo("That won't do much here, but if you want the power to create change with us, send us an email at <a href='mailto:weare@changecode.io'>weare@changecode.io</a> expressing your interest and how you think you can help!", {keepWords: true})
+        this.echo("That won't do much here, but if you want the power to create change with us, send us an email at <a href='mailto:weare@changecode.io'>weare@changecode.io</a> expressing your interest and how you think you can help!", {raw: true}, {keepWords: true})
     },
     tech: function() {
-        this.echo("CC works primarily with web3 technology including blockchains, zero-knowledge proofs, self-sovereign identity, and decentralized storage. \n\nThis website was built using Framer and this terminal was developed using jQuery Terminal. ", {keepWords: true})
+        this.echo("CC works primarily with web3 technology including blockchains, zero-knowledge proofs, self-sovereign identity, and decentralized storage. \n\nThis website was built using Framer and this terminal was developed using jQuery Terminal.", {keepWords: true})
     },
     reading_list: function() {
-        this.echo("Predictably Irrational: The Hidden Forces That Shape Our Decisions | Dan Ariely | <a href=https://www.goodreads.com/en/book/show/1713426>link</a> <br>Inspired: How to Create Tech Products Customers Love | Marty Cagan | <a href='https://www.goodreads.com/en/book/show/35249663'>link</a> <br>The Infinite Machine: How an Army of Crypto-hackers Is Building the Next Internet with Ethereum | Camila Russo | <a href='https://www.goodreads.com/en/book/show/50175330'>link</a> <br>Capital in the Twenty First Century | Thomas Piketty | | <a href='https://www.goodreads.com/en/book/show/18736925'>link</a>", {raw: true}, {keepWords: true})
+        this.echo("Predictably Irrational: The Hidden Forces That Shape Our Decisions | Dan Ariely | <a href=https://www.goodreads.com/en/book/show/1713426, target=_blank>link</a> <br><br>Inspired: How to Create Tech Products Customers Love | Marty Cagan | <a href=https://www.goodreads.com/en/book/show/35249663, target=_blank>link</a> <br><br>The Infinite Machine: How an Army of Crypto-hackers Is Building the Next Internet with Ethereum | Camila Russo | <a href=https://www.goodreads.com/en/book/show/50175330, target=_blank>link</a> <br><br>Capital in the Twenty First Century | Thomas Piketty | | <a href=https://www.goodreads.com/en/book/show/18736925, target=_blank>link</a>", {raw: true}, {keepWords: true})
     },
 }
 
@@ -42,13 +42,13 @@ function initTerminal() {
 
     var ascii =
 
-".   ____ _                               ____          _               \n"+
-".  / ___| |__   __ _ _ __   __ _  ___   / ___|___   __| | ___          \n"+
-". | |   | '_ \\ / _` | '_ \\ / _` |/ _ \\ | |   / _ \\ / _` |/ _ \\    \n"+
-". | |___| | | | (_| | | | | (_| |  __/ | |__| (_) | (_| |  __/         \n"+
-".  \\____|_| |_|\\__,_|_| |_|\\__, |\\___|  \\____\\___/ \\__,_|\\___| \n"+
-".                          |___/                                       \n"+
-"                                                                       \n"
+".   ____ _                               ____          _              \n"+
+".  / ___| |__   __ _ _ __   __ _  ___   / ___|___   __| | ___         \n"+
+". | |   | '_ \\ / _` | '_ \\ / _` |/ _ \\ | |   / _ \\ / _` |/ _ \\   \n"+
+". | |___| | | | (_| | | | | (_| |  __/ | |__| (_) | (_| |  __/        \n"+
+".  \\____|_| |_|\\__,_|_| |_|\\__, |\\___|  \\____\\___/ \\__,_|\\___|\n"+
+".                          |___/                                      \n"+
+"                                                                      \n"
 
 
     var greetingsText = "[[b;#0975DC;]Building the every economy]\n\n\nWelcome human, type [[b;#0975DC;]start] to list available commands and begin learning more about Change Code.\n\n"
@@ -64,7 +64,7 @@ function initTerminal() {
         },
         greetings: function() {
             let width = window.innerWidth;
-            const breakPoint = 531;
+            const breakPoint = 580;
             if(width < breakPoint) {
                 this.echo(greetingsText, {keepWords: true});
         } else {
